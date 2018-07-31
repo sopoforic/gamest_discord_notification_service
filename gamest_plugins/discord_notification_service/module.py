34980d3e8c8520fbfb3a218a3fa0138d114c35a0
Tracy Poff <tracy.poff@gmail.com>
2018-07-31 16:30:21 -0400

    Disallow blank user names. Fix hint text for discord ID.

--- gamest_plugins/discord_notification_service/module.py
+++ gamest_plugins/discord_notification_service/module.py
@@ -33,12 +33,18 @@ class DiscordNotificationService(NotificationService):
     @classmethod
     def get_settings_template(cls):
         d = super().get_settings_template()
+
         d[(cls.__name__, 'webhooks')] = {
             'name' : 'Webhooks',
             'type' : 'list',
             'hint' : ("The URL of the webhook you wish to notify. To notify multiple webhooks, put "
                       "one webhook per line."),
         }
+
+        def validate_user_name(name):
+            if len(name) == 0:
+                raise ValueError("User name must not be blank.")
+
         d[(cls.__name__, 'user_name')] = {
             'name' : 'User name',
             'type' : 'text',
@@ -46,13 +52,16 @@ class DiscordNotificationService(NotificationService):
             'hint' : ("You should probably set this to your discord nickname, but it can be set to "
                       "anything. If you set a Discord ID then whenever a notification @mentions "
                       "you, it will still use your discord nickname instead of what you set here."),
+            'validate' : validate_user_name,
         }
+
         d[(cls.__name__, 'discord_id')] = {
             'name' : 'Discord ID',
             'type' : 'text',
-            'hint' : ("You should probably set this to your discord nickname, but it can be set to "
-                      "anything. If you also set discord_id then whenever a notification @mentions "
-                      "you, it will still use your discord nickname instead of what you set here."),
+            'hint' : ("You can get your discord ID by enabling Developer Mode in discord's "
+                      "settings, then right-clicking your name in the user list and clicking 'Copy "
+                      "ID'. If you set this, then any notifications sent to discord that include "
+                      "your username will @mention you."),
         }
         return d
 
